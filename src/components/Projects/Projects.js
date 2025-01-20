8b6a7773e46ff1f2bc758c91ea3cb9d1d0410b92
sokhy009 <sokhykert@gmail.com>
2025-01-20 10:47:06 +0700

    myportfoilo

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/thirt.png";
+import emotion from "../../Assets/Projects/product-managment.png";
+import editor from "../../Assets/Projects/login-form.png";
+import chatify from "../../Assets/Projects/oderfoood.png";
+import suicide from "../../Assets/Projects/productcp.png";
+import bitsOfCode from "../../Assets/Projects/compuer.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="FOOD ORDER CARD"
+              description=""
+              ghLink="https://github.com/K31YY"
+              demoLink="https://chatify-49.keiyy.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="COMPUTERSHOP"
+              description=""
+              ghLink="https://github.com/K31YY"
+              demoLink="https://blogs.keiyy.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="LOGIN ANIMATION"
+              description=""
+              ghLink="https://github.com/K31YY"
+              demoLink="https://editor.keiyy.tech/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="CEATIVE PRODUCT"
+              description=""
+              ghLink="https://github.com/K31YY"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="COMPUTER PRODUCT"
+              description="."
+              ghLink="https://github.com/K31YY"
+              // demoLink="https://www.youtube.com" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="PRODUCT MANAGMENT"
+              description=""
+              ghLink="https://github.com/K31YY"
+              // demoLink="https://blogs.keiyy.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
