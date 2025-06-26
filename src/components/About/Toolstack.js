323979d53ba483d916797cbbb6318a8f89d8ba04
kheourtsokhy <naknak6412@gmail.com>
2025-06-26 19:31:00 +0700

    Edit Resume

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { DiEclipse, DiIntellij } from "react-icons/di";
 import {
   SiVisualstudiocode,
   SiPostman,
   SiSlack,
-  SiVercel,
-  SiMacos,
+  SiIntellijidea,
+  SiWindows,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
@@ -21,10 +22,7 @@ function Toolstack() {
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiIntellijidea />
       </Col>
     </Row>
   );
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,13 +24,13 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Java, Kotlin, Flutter and Android Studio. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Mobile Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Blockchain.
@@ -39,7 +39,7 @@ function Home2() {
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Dart</b> and
               <i>
                 <b className="purple">
                   {" "}
@@ -48,7 +48,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> Flutter </b>
               </i>
             </p>
           </Col>
